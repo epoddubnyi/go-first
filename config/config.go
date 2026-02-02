04bf1ec8105c79009c1bdc2571b4c003319a6f4d
epodd <johnyjust69@gmail.com>
2026-02-02 18:14:48 +0100

    fix: env

--- config/config.go
+++ config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/ilyakaznacheev/cleanenv"
 )
@@ -12,25 +11,25 @@ type DBConfig struct {
 	Port     string `env:"DB_PORT" yaml:"port"`
 	User     string `env:"DB_USER" yaml:"user"`
 	Password string `env:"DB_PASSWORD" yaml:"password"`
+	Name     string `env:"DB_NAME" yaml:"name"`
 }
 
 var Dsn string
 
 func LoadConfig() (*DBConfig, error) {
-	fmt.Println(os.Getenv("DB_USER"))
 	cfg := &DBConfig{}
 
-	if err := cleanenv.ReadEnv(&cfg); err != nil {
+	if err := cleanenv.ReadEnv(cfg); err != nil {
 		return nil, err
 	}
 
 	Dsn = fmt.Sprintf(
 		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASS"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_NAME"),
+		cfg.User,
+		cfg.Password,
+		cfg.Host,
+		cfg.Port,
+		cfg.Name,
 	)
 
 	return cfg, nil
